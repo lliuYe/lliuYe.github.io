91c36050f3403d5b02fd49d64339554983247851
lliuYe <llye66@qq.com>
2019-05-24 23:37:03 +0800

    2019/5/24/23:37

--- js/index.js
+++ js/index.js
@@ -4,4 +4,13 @@
  * @date    2019-05-24 21:52:00
  * @version $Id$
  */
-
+window.onload = function() {
+    var _hmt = _hmt || [];
+    (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?d6868e51a735ad30325f9086f6aa2653";
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(hm, s);
+    })();
+    alert("测试代码");
+}
